e7c3ac1304e320b00ccc04c0d8c4ca828c9f907a
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-12-16 00:38:10 -0800

    Implement task toggle and delete functionality with state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -14,14 +15,42 @@ const TASKS = [
   },
 ];
 
+const toggleTask = (task) => {
+  return { ...task, isComplete: !task.isComplete };
+};
+
 const App = () => {
+  const [taskData, setTaskData] = useState(TASKS);
+
+  const handleToggleCompleteTask = (id) => {
+    setTaskData((taskData) => {
+      return taskData.map((task) => {
+        if (task.id === id) {
+          return toggleTask(task);
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const handleDeleteTask = (id) => {
+    setTaskData((taskData) => {
+      return taskData.filter((task) => task.id !== id);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <TaskList
+          tasks={taskData}
+          onToggleCompleteTask={handleToggleCompleteTask}
+          onDeleteTask={handleDeleteTask}
+        />
       </main>
     </div>
   );
